d0c85e54840d34cc2f41103b8ebda46664674c9f
KibaekJeong <47020010+KibaekJeong@users.noreply.github.com>
2019-02-14 16:55:08 +0900

    data collection update
    updated audio analysis data collection

--- getdata.py
+++ getdata.py
@@ -5,3 +5,22 @@ client_credentials_manager = SpotifyClientCredentials(client_id="f2d11bfce2cf4ba
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 # get access token for spotify
 token = client_credentials_manager.get_access_token()
+tracks = ["spotify:track:7BKLCZ1jbUBVqRi2FVlTVw"]
+track = "7BKLCZ1jbUBVqRi2FVlTVw"
+track_info = sp.tracks(tracks)
+track_info1 = track_info['tracks'][0]
+
+artist_uri = (track_info1['artists'][0]['uri'])
+artist_name = (track_info1['artists'][0]['name'])
+song_popularity = (track_info1['popularity'])
+
+artist = sp.artist(artist_uri)
+artist_popularity = artist['popularity']
+artist_followers =(artist['followers']['total'])
+
+audio_analysis = sp.audio_analysis(track)
+
+print(song_popularity)
+print(artist_popularity)
+print(audio_analysis['sections'])
+print(audio_analysis['segments'])
